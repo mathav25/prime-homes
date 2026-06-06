132b2caa3d253a7cf59dacbcf4f9892015f54ea2
MATHAVAN M R <115515679+mathav25@users.noreply.github.com>
2026-06-06 12:20:25 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const questions =
+document.querySelectorAll(".faq-question");
+
+questions.forEach(question => {
+
+    question.addEventListener("click", () => {
+
+        const answer =
+        question.nextElementSibling;
+
+        answer.classList.toggle("active");
+
+    });
+
+});
